a56fd7960e6426e67ba955b2a93687bce9960e55
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-29 10:23:26 +0330

    i use banner module in homepage

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,6 +1,11 @@
+import Banner from "../modules/Banner";
 import styles from "./HomePage.module.css";
 const HomePage = () => {
-  return <div className={styles.container}>HomePage HomePage</div>;
+  return (
+    <div className={styles.container}>
+      <Banner />
+    </div>
+  );
 };
 
 export default HomePage;
