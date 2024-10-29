26011f5f4d42d9626db93134098864a8c1e1fe2b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-29 10:24:59 +0330

    i use attributes component in home page and show in ui

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,9 +1,11 @@
+import Attributes from "../modules/Attributes";
 import Banner from "../modules/Banner";
 import styles from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={styles.container}>
       <Banner />
+      <Attributes />
     </div>
   );
 };
