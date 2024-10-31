796248d5142f1a1504648c92add9df56ebd1f9c1
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-31 10:07:16 +0330

    i now develope the details page first part

--- components/templates/DetailsPage.js
+++ components/templates/DetailsPage.js
@@ -1,6 +1,40 @@
-import style from "./DetailsPage.module.css"
-const DetailsPage = () => {
-  return <div>DetailsPage</div>;
+import styles from "./DetailsPage.module.css";
+import Location from "../icons/Location";
+import Dollar from "../icons/Dollar";
+const DetailsPage = ({
+  id,
+  anme,
+  price,
+  discount,
+  introduction,
+  details,
+  ingredients,
+  recipe,
+}) => {
+  return (
+    <div className={styles.container}>
+      <h2>Details</h2>
+      <div className={styles.subContainer}>
+        <div className={styles.banner}>
+          <img src={`/images/${id}.jpeg`} alt={name} />
+          <div>
+            {name}
+            <span className={styles.location}>
+              <Location />
+              {details[0].Cuisine}
+            </span>
+            <span className={styles.price}>
+              <Dollar />
+              {discount ? (price * (100 - discount)) / 100 : price}$
+            </span>
+            {discount ? (
+              <span className={styles.discount}> {discount}%OFF</span>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailsPage;
