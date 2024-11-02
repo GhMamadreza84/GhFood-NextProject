4ab9d0a93ad9ee9ac5ace2cce36472876b185dd5
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-11-02 13:00:31 +0330

    i now set the query in url in categories route

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,22 +1,40 @@
+import { useState } from "react";
 import styles from "./CategoriesPage.module.css";
+import { useRouter } from "next/router";
 const CategoriesPage = () => {
+  const router = useRouter();
+  const [query, setQuery] = useState({ difficulty: "", time: "" });
+  const changeHandler = (e) => {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  };
+  const searchHandler = () => {
+    console.log(query);
+    router.push({
+      pathname: "/categories",
+      query,
+    });
+  };
   return (
     <div className={styles.container}>
       <h2>Categories</h2>
       <div className={styles.subContainer}>
         <div className={styles.select}>
-          <select>
+          <select
+            value={query.difficulty}
+            name="difficulty"
+            onChange={changeHandler}
+          >
             <option>Dificulty</option>
-            <option>Easy</option>
-            <option>Medium</option>
-            <option>Hard</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
           </select>
-          <select>
-            <option>Cooking Time</option>
-            <option>More than 30 min</option>
-            <option>Less than 30 min</option>
+          <select value={query.time} name="time" onChange={changeHandler}>
+            <option value="">Cooking Time</option>
+            <option value="more">More than 30 min</option>
+            <option value="less">Less than 30 min</option>
           </select>
-          <button>Search</button>
+          <button onClick={searchHandler}>Search</button>
         </div>
       </div>
     </div>
